3d7688a2c9f79bf12957b71b61c877fe0facea4e
itsviniaguiar <viniciusluizaguiar@gmail.com>
2017-10-12 04:59:28 -0300

    veracamargo

---
+++ controle de finanças pessoais.sql
@@ -0,0 +1,32 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.26-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando dados para a tabela controle de financas pessoais.cadastrousuario: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `cadastrousuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cadastrousuario` ENABLE KEYS */;
+
+-- Copiando dados para a tabela controle de financas pessoais.movimentacaosaldo: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `movimentacaosaldo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movimentacaosaldo` ENABLE KEYS */;
+
+-- Copiando dados para a tabela controle de financas pessoais.notificacoes: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `notificacoes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notificacoes` ENABLE KEYS */;
+
+-- Copiando dados para a tabela controle de financas pessoais.personalizarsistema: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `personalizarsistema` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personalizarsistema` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
